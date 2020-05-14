907019b4d40c773eab01b34d02d410d2be479a09
azmal <azmalhu55ain@outlook.com>
2020-05-14 11:52:47 +0530

    set up for heruko

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherform.addEventListener('submit', (e) => {
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ""
 
-    fetch(`http://localhost:3000/weather?city=${location}`).then((response) => {
+    fetch(`/weather?city=${location}`).then((response) => {
         response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error
